fa320e40002550b63f28ca18a898fbee73e6db93
ahmedBou <instinctauto@gmail.com>
2020-12-08 15:34:01 +0100

    payement simulator

---
+++ brief1/app/PaymentSimuator.java
@@ -0,0 +1,42 @@
+package com.brief1.app;
+
+import java.util.Scanner;
+
+public class PaymentSimuator {
+	
+	public static void main(String[] args) {
+		System.out.println("Good morning!");
+		Scanner nameIn = new Scanner(System.in);
+		System.out.print("Please enter your firstname and lastname: ");
+		String name = nameIn.nextLine();
+		
+		System.out.println("You are welcome mr " + name);
+		
+		Scanner hourIn = new Scanner(System.in);
+		System.out.print("Please enter the number of hour worked :");
+		int numOfHours = hourIn.nextInt();
+		
+		Scanner tariffIn = new Scanner(System.in);
+		System.out.print("Please enter your rate per hour :");
+		double tariffPerHours = tariffIn.nextInt();
+		double tariffPerhours = tariffPerHours * (numOfHours-40);
+		double extraTariff = tariffPerHours + 0.5 * tariffPerHours;
+		
+		if(numOfHours < 40) {
+			double finalTarrif = (numOfHours * tariffPerHours);
+			System.out.println("the final tarrif is: "+ finalTarrif);	
+		}
+		
+		else if(numOfHours <= 60) {
+			System.out.println("your compensation is "+ extraTariff);
+			double finalTarrif = (numOfHours * tariffPerHours) + extraTariff;
+			System.out.println("the final tarrif is: "+ finalTarrif);
+		}
+
+		else {
+			System.out.println("wrong information");
+		}
+		
+	}
+
+}
