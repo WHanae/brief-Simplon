51230eb3c06757e4e3360ec79048187f114ff874
ahmedBou <instinctauto@gmail.com>
2020-12-18 11:26:01 +0100

    refactoring the code

--- onlineTest/app/Quizz.java
+++ onlineTest/app/Quizz.java
@@ -5,6 +5,8 @@ package com.onlineTest.app;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.*;
+import java.util.Timer;
 
 class Quizz extends JFrame implements ActionListener{
 	
@@ -14,20 +16,38 @@ class Quizz extends JFrame implements ActionListener{
 	String[] reponseCorrect= {"Compilé et interpreté", "Object", "commence par une majuscule","oui","oui", "Interface", 
 							  ".Class"," aucun des choix", "vrai", "final","this","surchargée","vrai","vrai","vrai", " a=10 b=0 Je suis à l'interieur de finally"};
 	
-	JButton b1;
-	JButton v1;
+	static JButton b1;
+	static JButton v1;
 	ButtonGroup bg;
 	int count=0, current=0;	
 
-	JLabel label  = new JLabel("");
+	JLabel label  = new JLabel("",SwingConstants.CENTER);
+	static JLabel label1  = new JLabel("");
 	
+	//JPanel panel = new JPanel();
+	static JTextArea textChrono = new JTextArea();
+	Font f = new Font("Serif", Font.BOLD, 24);
+
+	
+	static Timer timer = new Timer();
+	static int i = 300;
 	
 	Quizz(String s){
 		super(s);
+		//panel.add(textChrono);
+		textChrono.setBounds(250,5,80,30); 
+		textChrono.setFont(f);
+		add(textChrono);
 		l=new JLabel();
 		add(l);
 		
-		bg=new ButtonGroup();
+		label1.setBounds(180, 250, 250, 30);
+		add(label1); 
+		Font  f1 = new Font("Serif", Font.BOLD,18);
+		label1.setFont(f1);
+		label1.setForeground(Color.RED);
+		
+ 		bg=new ButtonGroup();
 		
 		for(int i=0; i< jb.length; i++){
 			jb[i]=new JRadioButton();
@@ -49,34 +69,101 @@ class Quizz extends JFrame implements ActionListener{
 		add(b1);
 		set();
 		
-		label.setBounds(500, 20, 150, 30);
+		label.setBounds(1000, 20, 150, 30);
 		add(label); 
 		
-		l.setBounds(30,40,450,20);
+		l.setBounds(350,60,2000,3400);
 		jb[0].setBounds(50,80,100,20);
 		jb[1].setBounds(50,110,100,20);
 		jb[2].setBounds(50,140,100,20);
-		b1.setBounds(100,240,100,30);
+		b1.setBounds(120,300,100,30);
 		
 		v1=new JButton("Valider");
 		v1.addActionListener(this);
 		add(v1);
 		set();
-		v1.setBounds(300,240,100,30);
+		v1.setBounds(350,300,100,30);
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(null);
 		setLocation(250,100);
 		setVisible(true);
-		setSize(600,350);
+		setSize(600,400);
 		
 	}
+	static TimerTask task = new TimerTask(){
+		public void run(){
+			String time = getTime(i);
+			textChrono.setText(time);
+			i--;
+		}
+	};
+	public static void runTimer(){
+		
+			timer.schedule(task, 0, 1000 );	
+			
+			
+	}
+	
 	
 
+
+	static String getTime(int sec)
+	{
+		
+		
+	    
+	    int minutes = 0;
+	    int seconds = 0;
+	    
+	    //pour afficher
+	    String strMins; 
+	    String strSecs;
+
+	    if(i==30) {
+	    	
+	    		label1.setText("Attention il te reste 30 sec");
+	    }
+	    if(i== 0) {
+	    	b1.setEnabled(false);
+	    	v1.setEnabled(false);
+	    	//System.exit(0);	
+	    }
+	   if (sec >= 60){
+		   
+	        minutes = sec / 60;
+	        seconds = sec % 60;
+	    }
+
+	    else if (sec < 60){
+	    	
+	        minutes = 0;
+	        seconds = sec;
+	    }
+	    
+	     
+
+	    if(seconds < 10)
+	    	strSecs = "0" + Integer.toString(seconds);
+	    else
+	    	strSecs = Integer.toString(seconds);
+	   
+	    if(minutes < 10)
+	   	 strMins = "0" + Integer.toString(minutes);
+	   else
+		   strMins = Integer.toString(minutes);
+	    
+	    	
+	        
+	    String time =strMins + ":" + strSecs;
+	    return time;
+	}
 	public void actionPerformed(ActionEvent e){
 		if(e.getSource()==b1){
 			current++;
-			set();	
+			//vider();
+			set();
+			
 			//v1.setEnabled(false);
 			if(current==4){
 				b1.setEnabled(false);
@@ -90,6 +177,7 @@ class Quizz extends JFrame implements ActionListener{
 				b1.setEnabled(true);
 				label.setText("");
 				label.setText("Score : "+count);
+				
 			}
 			if(current== 9) {
 				
@@ -99,6 +187,7 @@ class Quizz extends JFrame implements ActionListener{
 			if(current == 15) {
 				System.out.println("current" + current);
 				b1.setEnabled(false);
+				
 				label.setText("");
 				label.setText("Bravo!!!!!!!!!!");
 				
@@ -110,7 +199,7 @@ class Quizz extends JFrame implements ActionListener{
 				count += 20;
 			}
 		}
-		vider();
+		
 		System.out.println(" Score ="+count);
 		
 		
@@ -119,103 +208,95 @@ class Quizz extends JFrame implements ActionListener{
 	void set(){
 
 		if(current==0){
-			l.setText("Que1: JAVA est un langage");
+			l.setText("<html><h1 style='color:blue;'>Question 1 :</h1> JAVA est un langage</html>");
 			jb[0].setText("Compilé"); jb[1].setText("Interprété"); jb[2].setText("Compilé et interpreté");	
 		}
 		
 		if(current==1){
-			l.setText("Que2: Toutes les classes héritent de la classe");
+			l.setText("<html>Que2:<br> Toutes les classes héritent de la classe</html>");
 			jb[0].setText("Main");jb[1].setText("Object");jb[2].setText("AWT");
+			
 		}
 	
 		if(current==2){
-			l.setText("Que3: Par convention une classe");
+			l.setText("<html>Que3:<br> Par convention une classe</html>");
 			jb[0].setText("est en minuscule");jb[1].setText("commence par une majuscule");
 			jb[2].setText("est en majuscules");
 		}
 		
 		if(current==3){
-			l.setText("Que4: Est-ce que on peut avoir plusieurs constructeurs pour la même classe ");
+			l.setText("<html>Que4:<br> Est-ce que on peut avoir plusieurs constructeurs pour la même classe </html>");
 			jb[0].setText("oui");jb[1].setText("non");jb[2].setText(" ");
 		}
 		
 		if(current==4){
-			l.setText("Que5: Dans la ligne public class A implements B\", B est");
+			l.setText("Que5:<br> Dans la ligne public class A implements B\", B est");
 			jb[0].setText("oui");jb[1].setText("non");jb[2].setText(" ");	
 		}
 	
 
 		if(current==5){
-			l.setText("Que6: Après la compilation, un programme écrit en\r\n"
-					+ "JAVA, il se transforme en programme en bytecode\r\n"
-					+ "Quelle est l’extension du programme en\r\n"
-					+ "bytecode ?\r\n"
-					+ "");
+			l.setText("<html>Que6:<br> Après la compilation, un programme écrit en <br>"
+					+ "            JAVA, il se transforme en programme en bytecode<br>"
+					+ "            Quelle est l’extension du programme en<br>"
+					+ "            bytecode ?<br>"
+					+ "</html>");
 			jb[0].setText("aucun des choix");jb[1].setText(".JAVA");jb[2].setText(".Class");	
 		}
 		if(current==6){
-			l.setText("Que7: Class Test{\r\n"
-					+ "Public Test () {\r\n"
-					+ "System.out.println(”Bonjour”);}\r\n"
-					+ "public Test (int i) {\r\n"
-					+ "this();\r\n"
-					+ "System.out.println(”Nous sommes en ”+i+ ” !”);};\r\n"
-					+ "}\r\n"
-					+ "qu’affichera l’instruction suivante?\r\n"
-					+ "Test t1=new Test (2018)");
+			l.setText("<html>Que7:<br> Class Test{Public Test () {"
+					+ "System.out.println(”Bonjour”);}<br>"
+					+ "public Test (int i) {<br>"
+					+ "this();<br>"
+					+ "System.out.println(”Nous sommes en ”+i+ ” !”);};<br>"
+					+ "}<br>"
+					+ "qu’affichera l’instruction suivante?<br>"
+					+ "Test t1=new Test (2018)</html>");
 			jb[0].setText("aucun des choix");jb[1].setText("Bonjour Nous sommes en 2018");jb[2].setText("Nous sommes en 2018");
 		}
 	
 		if(current==7){
-			l.setText("Que8: Voici un constructeur de la classe Employee, y-at'il un problème Public void Employe(String n) Nom=n");
+			l.setText("<html>Que8:<br> Voici un constructeur de la classe Employee, y-at'il un problème Public void Employe(String n) Nom=n</html>");
 			jb[0].setText("vrai");jb[1].setText("faux");
 		}
 		if(current==8){
-			l.setText("Que9: Pour spécifier que la variable ne pourra plus être modifiée et doit être initialisée dès sa déclaration,\r\n"
-					+ "on la déclare comme une constante avec le mot\r\n"
-					+ "réservé\r\n"
-					+ "");
+			l.setText("<html>Que9:<br> Pour spécifier que la variable ne pourra plus être modifiée et doit être initialisée dès sa déclaration,on la déclare comme une constante avec le mot réservé</html>");
 			jb[0].setText("aucun des choix");jb[1].setText("final");jb[2].setText(" const");
 		}
 		
 		if(current==9){
-			l.setText("Que10: Dans la ligne \"public class A implements B\", B est");
+			l.setText("<html>Que10:<br> Dans la ligne \"public class A implements B\", B est</html>");
 			jb[0].setText("oui");jb[1].setText("non");jb[2].setText(" ");	
 		}
 		if(current==10){
-			l.setText("Que11: Dans une classe, on accède à ses variables grâce au\r\n"
-					+ "mot clé\r\n"
-					+ "");
+			l.setText("<html>Que11:<br> Dans une classe, on accède à ses variables grâce au mot clé</html>");
 			jb[0].setText("aucun choix");jb[1].setText("super");jb[2].setText("const");	
 		}
 		if(current==11){
-			l.setText("Que12: Toutes les classes héritent de la classe");
+			l.setText("<html>Que12:<br> Toutes les classes héritent de la classe</html>");
 			jb[0].setText("Main");jb[1].setText("Object");jb[2].setText("AWT");
 		}
 	
 		if(current==12){
-			l.setText("Que13: Par convention une classe");
+			l.setText("<html>Que13:<br> Par convention une classe</html>");
 			jb[0].setText("est en minuscule");jb[1].setText("commence par une majuscule");
 			jb[2].setText("est en majuscules");
 		}
 		if(current==13){
-			l.setText("Que14:calculerSalaire(int)\r\n"
-					+ "calculerSalaire(int, double)\r\n"
-					+ "La méthode calculerSalaire est : ");
+			l.setText("<html>Que14:<br> calculerSalaire(int)<br> calculerSalaire(int, double)<br> La méthode calculerSalaire est : </html>");
 			jb[0].setText("aucun des choix");jb[1].setText("surchargée");jb[2].setText("redéfinie");
 		}
 		
 		if(current==14){
-			l.setText("Que15: Une classe qui contient au moins une méthode\r\n"
-					+ "abstraite doit être déclarée abstraite.");
+			l.setText("<html>Que15:<br> Une classe qui contient au moins une méthode abstraite doit être déclarée abstraite.</html>");
 			jb[0].setText("vrai");jb[1].setText("faux");
 			
 		}
 		
 		
-		l.setBounds(30,40,450,20);
+		l.setBounds(60,30,850,100);
 		for(int i=0,j=0;i<=90;i+=30,j++)
-			jb[j].setBounds(50,80+i,200,20);
+			jb[j].setBounds(100,140+i,200,20);
 		}
 	
 	public void vider() {
@@ -225,7 +306,9 @@ class Quizz extends JFrame implements ActionListener{
 	}
 
 	public static void main(String s[]){
+		runTimer();	
 		new Quizz("Online Test Of Java");
+		
 		
 	}
 
